d281e4f98fca15d9f7177c28ad92db81fbf7db76
sop-kim <77212828+sop-kim@users.noreply.github.com>
2023-10-28 14:32:12 -0400

    require login for deleting post

--- project/app.py
+++ project/app.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from flask import Flask, g, render_template, request, session, \
                   flash, redirect, url_for, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
+from functools import wraps
 
 basedir = Path(__file__).resolve().parent
 
@@ -25,6 +25,16 @@ app.config.from_object(__name__)
 db = SQLAlchemy(app)
 
 from project import models
+
+# decorators
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            flash('Please log in.')
+            return jsonify({'status': 0, 'message': 'Please log in.'}), 401
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route('/')
@@ -71,17 +81,20 @@ def logout():
 
 
 @app.route('/delete/<int:post_id>', methods=['GET'])
+@login_required
 def delete_entry(post_id):
     """Deletes post from database."""
     result = {'status': 0, 'message': 'Error'}
     try:
-        db.session.query(models.Post).filter_by(id=post_id).delete()
+        new_id = post_id
+        db.session.query(models.Post).filter_by(id=new_id).delete()
         db.session.commit()
         result = {'status': 1, 'message': "Post Deleted"}
         flash('The entry was deleted.')
     except Exception as e:
         result = {'status': 0, 'message': repr(e)}
     return jsonify(result)
+
 
 @app.route('/search/', methods=['GET'])
 def search():
@@ -90,6 +103,8 @@ def search():
     if query:
         return render_template('search.html', entries=entries, query=query)
     return render_template('search.html')
+
+
 
 
 if __name__ == "__main__":
--- tests/app_test.py
+++ tests/app_test.py
@@ -81,3 +81,13 @@ def test_delete_message(client):
     rv = client.get('/delete/1')
     data = json.loads(rv.data)
     assert data["status"] == 1
+
+def test_delete_message(client):
+    """Ensure the messages are being deleted"""
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 1
